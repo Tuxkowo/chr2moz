bce44b262ae2ffe5451336029db55d363470795c
Guillaume Louvigny <glouvigny@users.noreply.github.com>
2015-01-05 02:34:18 +0100

    Create Chrome and Firefox from a manifest file with extra params (such as licence, mozilla extension id)

--- manifest.js
+++ manifest.js
@@ -1,32 +1,75 @@
 var fs = require('fs');
 var manifestFile = process.env.PWD + '/manifest.json';
+var manifestExtrasFile = process.env.PWD + '/manifest-extras.json';
 var packageFile = process.env.PWD + '/package.json';
 
-return fs.readFile(manifestFile, 'utf8', function (err, data) {
-    if (err) {
-        console.error('Error while loading file package.json');
-        return;
+var removeExtraneousNode = function (doc, extras) {
+  if (typeof extras === 'object') {
+    for (var i in extras) {
+      if (extras.hasOwnProperty(i) && doc[i] !== undefined) {
+        if (extras[i] === true) {
+          delete doc[i];
+        } else if (typeof extras === 'object') {
+          doc[i] = removeExtraneousNode(doc[i], extras[i]);
+        }
+      }
     }
+  }
 
-    var manifest = JSON.parse(data);
+  return doc;
+};
 
-    var pack = {
-      "name": manifest.name.toLowerCase()
-        .replace(/[^a-z0-9]/g, '-'),
-      "title": manifest.name,
-      "id": manifest.moz_id ||
-        "jid" + Math.random().toString(36).replace(/\./g, ''),
-      "description": manifest.name,
-      "author": manifest.author,
-      "version": manifest.version,
-      "licence": manifest.licence || "MIT",
+var createManifest = function (doc) {
+    var firefox_nodes = {
+      browser_action: {
+        scripts: true,
+        height: true,
+        width: true,
+      },
+      licence: true,
+      moz_id: true,
     };
 
-    return fs.writeFile(packageFile, JSON.stringify(pack),
+    doc = removeExtraneousNode(doc, firefox_nodes);
+
+    return fs.writeFile(manifestFile, JSON.stringify(doc, null, '  '),
+        function (err, data) {
+            if (err) {
+                console.error('Error while writing manifest.json');
+            }
+        }
+    );
+};
+
+var createPackage = function (extras) {
+    var pack = {
+      "name": extras.name.toLowerCase()
+        .replace(/[^a-z0-9]/g, '-'),
+      "title": extras.name,
+      "id": extras.moz_id ||
+        "jid" + Math.random().toString(36).replace(/\./g, ''),
+      "description": extras.name,
+      "author": extras.author,
+      "version": extras.version,
+      "licence": extras.licence || "No licence",
+    };
+
+    return fs.writeFile(packageFile, JSON.stringify(pack, null, '  '),
         function (err, data) {
             if (err) {
                 console.error('Error while writing package.json');
             }
         }
     );
+};
+
+
+return fs.readFile(manifestExtrasFile, 'utf8', function (err, data) {
+    if (err) {
+        console.error('Error while loading file manifest-extras.json');
+        return;
+    }
+
+    createPackage(JSON.parse(data));
+    createManifest(JSON.parse(data));
 });
